f924a81f6737448cbdc3f38bc5d6850f1d9a0792
snowbe11 <snowbe11@ncsoft.com>
2021-12-15 09:54:07 +0900

    heroku 가 ts 를 지원하지 않아서 js 로 다시 변환

---
+++ backend/app.js
@@ -0,0 +1,5 @@
+const host = process.env.HOST ? process.env.HOST : "localhost";
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3001;
+
+const server = require("./cors-proxy.js");
+server.start(host, port);
