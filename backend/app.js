4323158516e7fd8d41ddc5eee8d602d1f7351ea8
snowbe11 <snowbe11@ncsoft.com>
2021-12-15 10:23:52 +0900

    ts 로 다시 환원
    package.json 의  스크립트를 수정

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const host = process.env.HOST ? process.env.HOST : "localhost";
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3001;
 
-const server = require("./cors-proxy.js");
+const server = require("./proxy");
 server.start(host, port);
