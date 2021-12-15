5de8eb7ebca7be5cf2abbe33ee5a7b227b0f76c9
snowbe11 <snowbe11@ncsoft.com>
2021-12-15 09:36:56 +0900

    데이터 감추기

--- backend/app.ts
+++ backend/app.ts
@@ -1,4 +1,7 @@
 import ProxyServer from "./cors-proxy";
 
+const host = process.env.HOST ? process.env.HOST : "localhost";
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3001;
+
 const server = new ProxyServer();
-server.start("localhost", 3001);
+server.start(host, port);
