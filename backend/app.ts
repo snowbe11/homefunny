6e3a3c483dca4666fa85620a13380743416debf2
snowbe11 <snowbe11@ncsoft.com>
2021-12-17 14:52:10 +0900

    webgl 을 로드할 수 있는 서버사이드 코드 추가

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,11 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import http from "http";
 import { Server, Socket } from "socket.io";
 import cors from "cors";
+import apiRouter from "./router";
+import bindAll from "./logic/socket";
+import path from "path";
+import compression from "compression";
 
 const app = express();
 
@@ -10,9 +14,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // router setting
-import apiRouter from "./router";
-import bindAll from "./logic/socket";
 app.use("/", apiRouter);
+
+app.use(compression());
+
+const webglBinFolder: string = path.join(__dirname, "/client");
+app.use(express.static(webglBinFolder));
+
+app.use((req: Request, res: Response) => {
+  res.sendFile(path.join(webglBinFolder, "index.html"));
+
+  // const fileURL = webglBinFolder + "/index.html";
+  // const extname = path.extname("/index.html");
+
+  // fs.readFile(fileURL, function (err, data) {
+  //   if (err) {
+  //     res.writeHead(404, { "Content-Type": "text/html;charset=UTF8" });
+  //     res.end("404,Request file does not exist:" + fileURL);
+  //   }
+
+  //   getMime(extname, (mime: string) => {
+  //     res.writeHead(200, { "Content-Type": mime });
+  //     res.end(data);
+  //     console.log("extname:" + extname);
+  //   });
+  // });
+});
 
 const httpServer = http.createServer(app);
 
