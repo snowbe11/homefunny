2c8d68153ff3fa7477bdea6a24b143f3175d0ea0
snowpapa@home <snowpapa@gmail.com>
2022-01-10 14:44:19 +0900

    시간 입력 필드 추가

--- webapp/src/component/LogParser/index.tsx
+++ webapp/src/component/LogParser/index.tsx
@@ -1,35 +1,29 @@
 import { NameTag } from "component/NameTag";
+import { splitLogToContents } from "logic/api/misc";
 import React from "react";
 
 type LogParserProps = {
   log: string;
 };
 
-export type EventType = "bath" | "custom" | "ban";
-
 export const LogParser = ({ log }: LogParserProps) => {
-  //const newLog = [values.who, values.event, values.log].join("|");
-  const token = log.split("|");
-  const who = token[0];
-  const event = token[1] as EventType;
-  const logText = token[2];
-
+  const { name, type, text } = splitLogToContents(log);
   let message = "";
-  switch (event) {
+  switch (type) {
     case "bath":
       message = "화장실 쓰는 날";
       break;
     case "ban":
-      message = `${logText} 금지`;
+      message = `${text} 금지`;
       break;
     default:
-      message = logText;
+      message = text;
       break;
   }
 
   return (
     <span>
-      <NameTag name={who} />
+      <NameTag name={name} />
       {message}
     </span>
   );
